a12ebc7d0f2371fb9d61f3b532fada434e7ad014
ArielusX <llugo.vera.ll@gmail.com>
2022-02-24 12:35:52 +0100

    Primera parte

---
+++ src/JocdelaVida/Joc.java
@@ -0,0 +1,12 @@
+package JocdelaVida;
+
+public class Joc {
+	public static void inicialitzarTauler() {
+
+//es demanaran les dimensions (files i columnes) del tauler (que hauran de ser entre 4 i 10)
+//tauler mida definida, tot mort
+//sorteix cel vives
+//num aleatoris celula
+
+	}
+}
