f1c89614e66725e7d6e2817bc086a30fbb4d49bd
ArielusX <llugo.vera.ll@gmail.com>
2022-03-02 19:20:40 +0100

    Juego casi acabado

--- src/JocdelaVida/Joc.java
+++ src/JocdelaVida/Joc.java
@@ -9,7 +9,6 @@ public class Joc {
 	//sorteix cel vives
 	//num aleatoris celula
 	
-	static int [][]taula=null;
 	
 	static Scanner source = new Scanner (System.in);
 	
@@ -17,35 +16,57 @@ public class Joc {
 			
 		int[] dimension=dimensions();
 		
-		taula=new int[dimension[0]][dimension[1]];
+		int [][]taula=new int[dimension[0]][dimension[1]];
+		
+		taula=iniciarvuit(dimension,taula);
+		
+		int vives=randomvives(dimension);		
+		
+		//Coloca les cel·lules aleatoriament
+		taula=colocarcelulas(dimension,vives,taula);
+		
+		//Coloca les cel·lules en posicions determinades
+		//taula=colocarcelulasejemplo(dimension,taula);
 
+		int iterar=numiterar();
 		
-		for (int i = 0; i < dimension[0]; i++) {
-		for (int j = 0; j < dimension[1]; j++) { 
-			
-			taula[i][j] = 0;
+		taula=iteraciones(iterar,taula,dimension);
+		
+		print(dimension, taula);
 
-		}
-		}
-		
-		int vives=randomvives(dimension);
-		System.out.println(vives);
-		
-		colocarcelulas(dimension,vives);
-		
-		//random(dimension);
-		System.out.println("Las dimensiones del tablero son: "+dimension[0]+" por "+dimension[1]);
-		
-		print(dimension);
-		for(int i=0;i<100;i++) {
-			celules(dimension);
-			print(dimension);
-		}
-		
-		
-		
 	}
-	public static void colocarcelulas(int[] dim, int vives) {
+	public static int numiterar() {
+		System.out.println("Introduce el numero de iteraciones: ");
+		int n=0;
+		try {
+				n=source.nextInt();
+
+		}catch(Exception e){
+			System.out.println("Valor incorrecto");
+		}
+		return n;
+	}
+	
+	public static int[][] iteraciones(int iterar,int[][]taula, int []dimension) {
+		for(int i=0;i<iterar;i++) {
+			taula=celules(dimension,taula);
+			print(dimension,taula);
+		}
+		return taula;
+	}
+	
+	public static int [][] iniciarvuit(int[]dimension, int[][]taula){
+		for (int i = 0; i < dimension[0]; i++) {
+			for (int j = 0; j < dimension[1]; j++) { 
+				
+				taula[i][j] = 0;
+	
+			}
+		}
+		return taula;
+	}
+	
+	public static int[][] colocarcelulas(int[] dim, int vives, int[][]taula) {
 	
 		for(int i=0;i<vives;i++) {
 			int ran=random(dim);
@@ -58,6 +79,28 @@ public class Joc {
 				i--;
 			}
 		}
+		return taula;
+	}
+	
+	public static int[][] colocarcelulasejemplo(int[] dim, int[][]taula) {
+		
+		taula[0][4]=1;
+		taula[1][4]=1;
+		taula[2][4]=1;
+		
+		taula[4][0]=1;
+		taula[4][1]=1;
+		taula[4][2]=1;
+		
+		taula[6][4]=1;
+		taula[7][4]=1;
+		taula[8][4]=1;
+		
+		taula[4][6]=1;
+		taula[4][7]=1;
+		taula[4][8]=1;
+		
+		return taula;
 	}
 	
 	public static int randomvives(int[] dim) {
@@ -66,6 +109,7 @@ public class Joc {
 		return ran;
 		
 	}
+	
 	public static int random(int[] dim) {
 		int max=dim[0]*dim[1];
 		int ran=(int)Math.floor(Math.random()*(max));
@@ -74,7 +118,6 @@ public class Joc {
 	}
 	
 	public static int[] dimensions() {
-		System.out.println("HEY");
 		int t =0;
 		int[] xy =new int [2];
 		
@@ -89,7 +132,7 @@ public class Joc {
 				boolean valid=false;
 				while(!valid) {
 					n=source.nextInt();
-					if(n<4 || n>10) {
+					if(n<4 || n>101) {
 						System.out.println("El numero debe estar entre el 4 y el 10");
 					}else {
 						xy[t]=n;
@@ -104,48 +147,49 @@ public class Joc {
 				System.out.println("Valor incorrecto");
 			}
 		}
-		
+		System.out.println("Las dimensiones del tablero son: "+xy[0]+" por "+xy[1]);
 		return xy;
 	}
 	
-	public static void celules(int[] dim) {
+	public static int[][] celules(int[] dim, int[][]taula) {
 
 		int vecinas =0;
+		
+		int [][]taulaaux=new int [dim[0]][dim[1]];
 		
 		for (int i = 0; i < dim[0]; i++) {
 			for (int j = 0; j < dim[1]; j++) { 
 				vecinas =0;
 				for (int n = -1; n < 2; n++) {
 					for (int  m= -1; m < 2; m++) {
-						if(n+i>=0 &&n+i<dim[0] && m+j>=0 && m+j<dim[1]&& (i!=0 && j!=0)) {
+						if(n+i>=0 &&n+i<dim[0] && m+j>=0 && m+j<dim[1]&& !(n==0 && m==0)) {
+
 							if(taula[i+n][j+m]==1) {
 								vecinas++;
-
 							}
 						}
-						
 					}	
 				}
-			
 				if(taula[i][j]==0) {
 					if(vecinas==3) {
-						taula[i][j]=1;
+						taulaaux[i][j]=1;
 					}else {
-						taula[i][j]=0;
+						taulaaux[i][j]=0;
 					}
 				}else if(taula[i][j]==1) {
 					if(vecinas==3 || vecinas==2) {
-						taula[i][j]=1;
+						taulaaux[i][j]=1;
 					}else {
-						taula[i][j]=0;
+						taulaaux[i][j]=0;
 					}
 				}
 			}
 		}
+		taula=taulaaux.clone();
+		return taula;
 	}
 		
-	
-	public static int print(int []xy) {
+	public static int print(int []xy, int[][]taula) {
 		int fila=0;
 		int col=0;
 		int res=0;
@@ -187,10 +231,5 @@ public class Joc {
 		}
 		return res;
 	}
-
-	
-	
-	
-	
 }
 
