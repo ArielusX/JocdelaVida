9574d6c9f914a2ee2c6f7c6dc68fbf03f8a073c6
ArielusX <llugo.vera.ll@gmail.com>
2022-03-08 18:50:52 +0100

    TestClonar casi, cambiar parametros

--- src/JocdelaVida/JUNIT+JOC/ClonarTest.java
+++ src/JocdelaVida/JUNIT+JOC/ClonarTest.java
@@ -13,12 +13,9 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class ClonarTest {
 
-	
+	private int [] dim;
 	private int [][] ini;
 	private int [][] fin;
-	private int [] dim;
-	private int [][] cas;
-	private boolean[] bool;
 	
 	private static int [] [] [] original = { 
 			{
@@ -94,71 +91,31 @@ public class ClonarTest {
 			}
 
 };
-	private static int [] [] [] caso = { 
-			{
-				{0,0,0,0,4,0,8,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0}
-			},
-			{ 
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,1,0,0,0,0}
-			
-			},
-			{ 	
-				{2,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,1,0,0,5}
-			}
-
-};
 	private static int [][] dimensiones = { 
 			
 			{9,9},
 			{9,9},
 			{9,9}
 	};
-	private static boolean []booleano = {true,true,true};
-	public ClonarTest  (int [] dimensions,int[][]inicial, int[][]fina,int[][]cas,boolean []boolea) {
+	public ClonarTest  (int [] dimensions,int[][]inicial, int[][]fina) {
 		this.dim =dimensions;
 		this.ini=inicial;
 		this.fin=fina;
-		this.cas=cas;
-		this.bool=boolea;
 	}
 	@Parameters
 	public static Collection<Object[]> numeros() {
 		
 		return Arrays.asList(new Object[][] {
-			{dimensiones[0],original[0], copia[0],caso[0],booleano[0]},
-			{dimensiones[1],original[1], copia[1],caso[1],booleano[1]},
-			{dimensiones[2],original[2], copia[2],caso[2],booleano[2]}
+			{dimensiones[0],original[0], copia[0]},
+			{dimensiones[1],original[1], copia[1]},
+			{dimensiones[2],original[2], copia[2]}
 			
 		});
 	}
 	@Test
 	public void testClonarTauler() {
-		boolean res = (cas ==Joc.clonartaula(dim,ini,fin));
-		assertEquals(bool, res);
+		int [][] res = Joc.clonartaula(dim,ini,fin);
+		assertArrayEquals(ini, res);
 	}
 
 }
